da303803470a364dfe62755ed2b73581c98292e9
kafkaaaa28 <kafkafj1@gmail.com>
2025-05-14 16:37:19 +0700

    perbaikiii vercel

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // server.js
 const app = require('./app');
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, '0.0.0.0', () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+module.exports = app;
