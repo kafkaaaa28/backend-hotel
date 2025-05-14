67cc7abca0675e628ae06bb14c0b541ebb7bea7a
kafkaaaa28 <kafkafj1@gmail.com>
2025-05-14 16:14:29 +0700

    perbaiki vercel.json

--- config/db.js
+++ config/db.js
@@ -12,4 +12,38 @@ const pool = mysql.createPool({
   queueLimit: 0,
   ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : null,
 });
+async function testConnection() {
+  try {
+    const connection = await pool.getConnection();
+    console.log('✅ Koneksi ke database berhasil!');
+    connection.release();
+  } catch (err) {
+    console.error('❌ Gagal koneksi ke database:', err.message);
+  }
+}
+testConnection();
 module.exports = pool;
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: '127.0.0.1',
+//   user: 'root',
+//   password: '',
+//   database: 'auth_system',
+//   port: 3306,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+//   ssl: false,
+// });
+// async function testConnection() {
+//   try {
+//     const connection = await pool.getConnection();
+//     console.log('✅ Koneksi ke database berhasil!');
+//     connection.release();
+//   } catch (err) {
+//     console.error('❌ Gagal koneksi ke database:', err.message);
+//   }
+// }
+// testConnection();
+// module.exports = pool;
