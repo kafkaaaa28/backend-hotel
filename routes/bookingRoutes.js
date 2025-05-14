2f8ddf5c6e95dac8b6a0e2f274f7ba7e2275936d
kafkaaaa28 <kafkafj1@gmail.com>
2025-05-14 16:23:59 +0700

    tes vercel.json

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const upload = require('../config/multer'); // Untuk handle file upload
+const upload = require('../config/multer');
 
 router.post('/', authMiddleware, upload.single('payment_proof'), bookingController.createBooking);
 router.get('/my-bookings', authMiddleware, bookingController.getUserBookings);
